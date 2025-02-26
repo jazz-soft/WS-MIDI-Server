4276df9f1bdd9b897052f288e7e5346b58256ec8
Sema <jazz-dev@jazz-soft.net>
2025-02-26 18:12:16 -0500

    MIDI watcher

--- midi.js
+++ midi.js
@@ -11,15 +11,34 @@ const virtual_out = new JZZ.Widget();
 JZZ().and(function() {
     var p;
     var info = JZZ().info();
-    for (p of info.inputs) midiws.addMidiIn(p.id, JZZ().openMidiIn(p.id));
-    for (p of info.outputs) midiws.addMidiOut(p.id, JZZ().openMidiOut(p.id));
-    midiws.addMidiIn('Virtual', virtual_in);
-    midiws.addMidiOut('Web Audio', virtual_out);
+    for (p of info.inputs) addMidiIn(p.name, JZZ().openMidiIn(p.name));
+    for (p of info.outputs) addMidiOut(p.name, JZZ().openMidiOut(p.name));
+    addMidiIn('Virtual', virtual_in);
+    addMidiOut('Web Audio', virtual_out);
 }).or('Cannot start MIDI engine!');
+JZZ().onChange(function(x) {
+  var p;
+  for (p of x.inputs.added) addMidiIn(p.name, JZZ().openMidiIn(p.name));
+  for (p of x.outputs.added) addMidiOut(p.name, JZZ().openMidiOut(p.name));
+  for (p of x.inputs.removed) removeMidiIn(p.name);
+  for (p of x.outputs.removed) removeMidiOut(p.name);
+});
+
+function addMidiIn(name, port) {
+  midiws.addMidiIn(name, port);
+}
+function addMidiOut(name, port) {
+  midiws.addMidiOut(name, port);
+}
+function removeMidiIn(name, port) {
+  midiws.removeMidiIn(name, port);
+}
+function removeMidiOut(name, port) {
+  midiws.removeMidiOut(name, port);
+}
 
 module.exports = {
   wss: function (req, socket, head) {
-    console.log('Req URL:', req.url);
     if (req.url == '/') {
       wss1.handleUpgrade(req, socket, head, function(ws) { wss1.emit('connection', ws, req); });
     }
