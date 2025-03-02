5bc1e649cf67da3a2c916eb969192f31882c27a1
Sema <jazz-dev@jazz-soft.net>
2025-03-02 00:22:26 -0500

    Virtual Piano and Web Audio synth

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ WebSocket MIDI Server
 `npm install`  
 `npm start`
 
+## How it works
+The application exposes the MIDI ports available on your system,
+plus Virtual Piano and Web Audio synth on the Master page.
+
 ## See also
 https://github.com/jazz-soft/JZZ-midi-WS
--- midi.js
+++ midi.js
@@ -5,6 +5,8 @@ const wss1 = new ws.WebSocketServer({ noServer: true });
 const wss2 = new ws.WebSocketServer({ noServer: true });
 const midiws = new JZZ.WS.Server(wss1);
 
+const inputs = {};
+const outputs = {};
 const virtual_in = new JZZ.Widget();
 const virtual_out = new JZZ.Widget();
 
@@ -20,36 +22,78 @@ wss2.on('connection', function connection(ws) {
     timeout = setTimeout(process.exit, 1000);
   });
   ws.on('message', function message(data) {
+    try {
+      data = JSON.parse(data);
+      if (data.refresh) refresh();
+      else if (data.midi) virtual_in.send(JZZ.WS.decode(data.midi));
+    }
+    catch (e) {/**/}
   });
 });
 
+inputs['Virtual Piano'] = { port: virtual_in };
+midiws.addMidiIn('Virtual Piano', virtual_in);
+outputs['Web Audio'] = { port: virtual_out };
+midiws.addMidiOut('Web Audio', virtual_out);
+virtual_out._receive = function(msg) { master.send(JSON.stringify({ midi: JZZ.WS.encode(msg) })); };
+
 JZZ().and(function() {
-    var p;
-    var info = JZZ().info();
-    for (p of info.inputs) addMidiIn(p.name, JZZ().openMidiIn(p.name));
-    for (p of info.outputs) addMidiOut(p.name, JZZ().openMidiOut(p.name));
-    addMidiIn('Virtual', virtual_in);
-    addMidiOut('Web Audio', virtual_out);
-}).or('Cannot start MIDI engine!');
+  var p;
+  var info = JZZ().info();
+  for (p of info.inputs) addMidiIn(p.name);
+  for (p of info.outputs) addMidiOut(p.name);
+}).or(function() {
+  console.log('Cannot start MIDI engine!');
+});
 JZZ().onChange(function(x) {
   var p;
-  for (p of x.inputs.added) addMidiIn(p.name, JZZ().openMidiIn(p.name));
-  for (p of x.outputs.added) addMidiOut(p.name, JZZ().openMidiOut(p.name));
+  for (p of x.inputs.added) addMidiIn(p.name);
+  for (p of x.outputs.added) addMidiOut(p.name);
   for (p of x.inputs.removed) removeMidiIn(p.name);
   for (p of x.outputs.removed) removeMidiOut(p.name);
 });
 
-function addMidiIn(name, port) {
-  midiws.addMidiIn(name, port);
+function addMidiIn(name) {
+  if (inputs[name]) return;
+  inputs[name] = {};
+  JZZ().openMidiIn(name).and(function() {
+    inputs[name].port = this;
+    midiws.addMidiIn(name, this);
+    refresh();
+  }).or(function() {
+    console.log('Cannot open MIDI input:', name);
+    refresh();
+  });
 }
-function addMidiOut(name, port) {
-  midiws.addMidiOut(name, port);
+function addMidiOut(name) {
+  if (outputs[name]) return;
+  outputs[name] = {};
+  JZZ().openMidiOut(name).and(function() {
+    outputs[name].port = this;
+    midiws.addMidiOut(name, this);
+    refresh();
+  }).or(function() {
+    console.log('Cannot open MIDI output:', name);
+    refresh();
+  });
 }
-function removeMidiIn(name, port) {
-  midiws.removeMidiIn(name, port);
+function removeMidiIn(name) {
+  midiws.removeMidiIn(name);
+  delete inputs[name];
+  refresh();
 }
-function removeMidiOut(name, port) {
-  midiws.removeMidiOut(name, port);
+function removeMidiOut(name) {
+  midiws.removeMidiOut(name);
+  delete outputs[name];
+  refresh();
+}
+
+function refresh() {
+  if (!master) return;
+  var n, inp = {}, outp = {};
+  for (n of Object.keys(inputs)) inp[n] = { open: !!inputs[n].port, hide: !!inputs[n].hide };
+  for (n of Object.keys(outputs)) outp[n] = { open: !!outputs[n].port, hide: !!outputs[n].hide };
+  master.send(JSON.stringify({ info: { inputs: inp, outputs: outp }}));
 }
 
 module.exports = {
